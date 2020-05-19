8a583347b6c59e0f7ba67f484073bd9e5345b7d5
Samuel Poirier <samuel.poirier@beslogic.com>
2020-05-18 22:26:50 -0400

    Round delta

---
+++ build/precache-manifest.d99bdcf43ac36d0267665dbacf3be5f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91980c2a31b6e5b6c350fa2965dad348",
+    "url": "/wealthica-passiv-rebalance/build/index.html"
+  },
+  {
+    "revision": "3d9f0432ef153a51d315",
+    "url": "/wealthica-passiv-rebalance/build/static/css/main.645856b7.chunk.css"
+  },
+  {
+    "revision": "b7c78db46c32d72cd913",
+    "url": "/wealthica-passiv-rebalance/build/static/js/2.f3659ce9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wealthica-passiv-rebalance/build/static/js/2.f3659ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d9f0432ef153a51d315",
+    "url": "/wealthica-passiv-rebalance/build/static/js/main.25e71d2b.chunk.js"
+  },
+  {
+    "revision": "f42002e80a9b09c71898",
+    "url": "/wealthica-passiv-rebalance/build/static/js/runtime-main.b2626472.js"
+  }
+]);
